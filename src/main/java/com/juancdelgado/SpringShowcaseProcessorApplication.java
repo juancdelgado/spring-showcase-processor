9ffec1ba20da510767fb7618be05f5ceb6d3a9ab
Juan Delgado <juandelgado@Juans-MacBook-Pro.local>
2022-04-23 14:11:16 -0500

    iniital project

---
+++ src/main/java/com/juancdelgado/SpringShowcaseProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.juancdelgado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringShowcaseProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringShowcaseProcessorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juancdelgado/SpringShowcaseProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juancdelgado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringShowcaseProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
